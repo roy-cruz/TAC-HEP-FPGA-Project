427b5bd5d4254d6c7cb61515193f6c1972c596fa
Roy Cruz <rfcruz@wisc.edu>
2026-05-06 16:00:54 -0500

    Rm unnecessary PUPPI loader

--- src/embedding/dataloader.py
+++ src/embedding/dataloader.py
@@ -1,27 +1,5 @@
 import torch
 
-class PUPPIDataset(torch.utils.data.Dataset):
-    def __init__(
-            self, 
-            data: torch.Tensor, 
-            labels: torch.Tensor, 
-            device: torch.device
-        ):
-        self.data = data
-        self.device = device
-        self.labels = labels  
-    
-    def __len__(self):
-        return self.data.shape[0]
-    
-    def make_padding_mask(self, x):
-        return x[:, 0] == 0  
-
-    def __getitem__(self, idx):
-        to_return = (self.data[idx], self.make_padding_mask(self.data[idx]))
-        to_return += (self.labels[idx],) if self.labels is not None else ()
-        return to_return
-
 class PFCandsDataset(torch.utils.data.Dataset):
     def __init__(
             self, 
